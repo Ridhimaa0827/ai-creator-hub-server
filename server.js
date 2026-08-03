caffb1133ae61dc9424afe5c4fe74d5d2dbddd4f
Ridhima Sharma <sharmaridhima0028@gmail.com>
2026-08-03 18:38:05 +0530

    Remove payment integration

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ import connectDB from "./config/db.js";
 import "./config/mail.js"
 import authRoutes from "./routes/authRoutes.js";
 import aiRoutes from "./routes/aiRoutes.js";
-import paymentRoutes from "./routes/paymentRoutes.js";
 
 connectDB();
 const app = express();
@@ -14,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/ai", aiRoutes);
-app.use("/api/payment", paymentRoutes);
+
 app.get("/", (req, res) => {
   res.send("API Running...");
 });
